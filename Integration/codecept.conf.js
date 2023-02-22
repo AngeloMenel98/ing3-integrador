3385b07a50f1b95e87acb7ec5a56ddd80615c830
Angelo Menel <angelomenel@hotmail.com>
2023-02-22 12:34:40 -0300

    Integration

---
+++ Integration/codecept.conf.js
@@ -0,0 +1,43 @@
+/** @type {CodeceptJS.MainConfig} */
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      url: 'https://frontend-production-0c0f.up.railway.app',
+      onRequest: () => {
+
+      },
+      show: false,
+      browser: 'chromium'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  mocha:  {
+    "reporterOptions": {
+      "codeceptjs-cli-reporter": {
+        "stdout": "-",
+        "options": {
+          "steps": true,
+        }
+      },
+      "mocha-junit-reporter": {
+        "stdout": "./output/console.log",
+        "options": {
+          "mochaFile": "./output/result.xml"
+        },
+        "attachments": true //add screenshot for a failed test
+		  }
+		}
+	},
+  plugins: {
+    testomatio: {
+      enabled: true,
+      require: '@testomatio/reporter/lib/adapter/codecept',
+      apiKey: process.env.TESTOMATIO,
+    }
+  },
+  name: 'IntegrationTestForIS3'
+}
